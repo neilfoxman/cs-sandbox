0746695adcbaa0dab0c1c0b26df783c2be60a925
neil <neilfoxman@gmail.com>
2017-08-31 20:54:41 -0700

    initing

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateSanbox
+{
+    class Program
+    {
+        // Declare delegate to be used
+        public delegate void Del(string message);
+
+        static void Main(string[] args)
+        {
+            // Instantiate delegate
+            Del handler = DelegateMethod;
+
+            // Call delegate
+            handler("yo");
+
+            // Pass delegate as a parameter
+            MethodWithCallback(1, 2, handler);
+
+            // Create delegates to non static methods
+            NonstaticClass nsc = new NonstaticClass();
+            Del d1 = nsc.NonstaticMethod1;
+            Del d2 = nsc.NonstaticMethod2;
+            Del d3 = DelegateMethod;
+
+            // Encapsulate several delegates in one delegate to invoke at same time
+            Del allMethodsDelegate = d1 + d2;
+            allMethodsDelegate += d3;
+            allMethodsDelegate("I have been passed");
+            allMethodsDelegate -= d1;
+            allMethodsDelegate("Second time around.");
+            Del simpleDelegate = allMethodsDelegate - d1 - d2;
+            simpleDelegate("Back to square one with me.");
+
+            // Get delegate properties
+            int invocationCount = allMethodsDelegate.GetInvocationList().GetLength(0);
+            handler(invocationCount.ToString());
+
+
+
+            Console.ReadKey();
+        }
+
+        // Create method for delegate
+        public static void DelegateMethod(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void MethodWithCallback(int param1, int param2, Del callback)
+        {
+            callback("Sum: " + (param1 + param2).ToString());
+        }
+    }
+
+    public class NonstaticClass
+    {
+        public void NonstaticMethod1(string message)
+        {
+            Console.WriteLine("NSM1 called");
+        }
+
+        public void NonstaticMethod2(string message)
+        {
+            Console.WriteLine("NSM2 called");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousSandbox
+{
+    class Program
+    {
+        delegate void Del(string s);
+
+        static void DumbMethod(string s)
+        {
+            Console.WriteLine("Dumb: " + s);
+        }
+
+        static void Main(string[] args)
+        {
+            // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/anonymous-functions
+
+            // Original syntax of delegate initialization
+            Del d1 = DumbMethod;
+            Del d1_5 = new Del(DumbMethod);
+
+            // Initialize delegate with inline code or "anonymous method"
+            Del d2 = delegate (string s) { Console.WriteLine("anonymous: " + s); };
+
+            // Initialize delegate with a lambda expression
+            Del d3 = (x) => { Console.WriteLine("labmda: " + x); };
+
+            Del allThaMethods = d1 + d1_5 + d2 + d3;
+            allThaMethods("string parameter");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
