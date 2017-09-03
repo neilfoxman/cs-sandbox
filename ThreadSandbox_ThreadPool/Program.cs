77b29a9c4560377bf75e895442f2a39585a15d4b
neil <neilfoxman@gmail.com>
2017-09-03 11:29:34 -0700

    added threadpool sandbox

---
+++ ThreadSandbox_ThreadPool/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSandbox_ThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an action - delegate that returns void
+            Action<object> action = (object obj) =>
+            {
+
+            };
+
+            //// Testing alternative declaration - works
+            //Action<object> action1 = delegate (object obj)
+            //{
+            //    Console.WriteLine("");
+            //};
+        }
+    }
+}
